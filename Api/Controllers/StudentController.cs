62d7095382279107b87e6c0d1a02981c58ed6ced
Stav-Kyriacou <101610510@student.swin.edu.au>
2022-05-14 13:29:51 +1000

    create api

---
+++ Api/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private List<Student> _studentList = new List<Student>()
+        {
+            new Student{Name = "George", Age = 21},
+            new Student{Name = "James", Age = 22},
+            new Student{Name = "John", Age = 23},
+            new Student{Name = "Sammy", Age = 24},
+        };
+        [HttpGet]
+        [EnableCors("MyPolicy")]
+        public IEnumerable<Student> Get()
+        {
+            return this._studentList;
+        }
+    }
+}
---
+++ Api/Student.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Api
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
